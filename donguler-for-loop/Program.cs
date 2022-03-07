ce9c6b16312ab927bfbeb7f8daf0899095ccba55
Alper Yalman <alperyalman@gmail.com>
2022-03-07 15:25:18 +0300

    for loop calismasi eklendi

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace donguler_for_loop // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            String result = "";
+
+           //Write even numbers
+            for (int i = 1; i<50; i++)
+            {
+                if(i%2 != 0)
+                    continue;
+                Console.Write(i + " - ");
+            }
+
+            Console.WriteLine();
+
+
+            for (int i = 1; i<=n; i++)
+            {
+                if(n == 0)
+                    break;
+                result += new String(' ', n-i) + new String('#',i) + "\n";
+            }
+            Console.WriteLine(result);
+
+                
+        }
+    }
+}
