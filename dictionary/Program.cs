14d29154d515f7ca9c5f763e66e90485e3b3b171
Alper Yalman <alperyalman@gmail.com>
2022-04-18 15:29:41 +0300

    Dictionary calismasi eklendi

---
+++ dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10,"Ayşe");
+            kullanicilar.Add(12,"Ali");
+            kullanicilar.Add(14,"Fatma");
+            kullanicilar.Add(15,"Mehmet");
+
+            Console.WriteLine(kullanicilar[12]);
+
+            foreach (var item in kullanicilar)
+                Console.WriteLine(item);
+
+            //Count
+            Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+            Console.WriteLine(kullanicilar.ContainsKey(12));
+            Console.WriteLine(kullanicilar.ContainsValue("Ahmet"));
+
+            //Remove
+            kullanicilar.Remove(12);
+            foreach (var item in kullanicilar)
+                Console.WriteLine(item);
+            
+            
+            foreach (var item in kullanicilar.Keys)
+                Console.WriteLine(item);
+            
+            foreach (var item in kullanicilar.Values)
+                Console.WriteLine(item);
+            
+            
+        }
+    }
+}
+
