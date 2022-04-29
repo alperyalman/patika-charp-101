f36e548a9828bf92160199e75a69fd71a27e9184
Alper Yalman <alperyalman@gmail.com>
2022-04-29 08:29:58 +0300

    Struct calismasi eklendi

---
+++ struct-kavrami/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace struct_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar=3;
+            dikdortgen.UzunKenar=4;
+            Console.WriteLine("Class alan hesabı: {0}",dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct; // = new Dikdortgen_Struct();
+            dikdortgen_struct.KisaKenar=3;
+            dikdortgen_struct.UzunKenar=4;
+            Console.WriteLine("Struct alan hesabı: {0}",dikdortgen.AlanHesapla());
+
+            
+            
+        }
+    }
+
+    class Dikdortgen 
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar*this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct 
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar*this.UzunKenar;
+        } 
+    }
+}
+
